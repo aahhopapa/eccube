cb45c8b9e10d68fab97cdf48d4a6ba7dff21add2
deho.chon <deho.chon@dragonagency.co.jp>
2022-07-27 17:46:40 +0900

    first commit

---
+++ app/Customize/Form/Extension/CompanyNameRequiredExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Customize\Form\Extension;
+
+
+use Eccube\Form\Type\Front\EntryType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+
+class CompanyNameRequiredExtension extends AbstractTypeExtension
+{ 
+  /**
+  * {@inheritdoc}
+  */
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $options = $builder->get('company_name')->getOptions();
+
+        $options['required'] = true;
+        $options['constraints'] = [ new NotBlank() ];
+        $options['attr']['placeholder'] = '会社名';
+
+        $builder->add('company_name', TextType::class, $options); 
+  }
+ 
+  /**
+   * {@inheritdoc}
+   */
+  public function getExtendedType()
+  {
+      return EntryType::class;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getExtendedTypes(): iterable
+  {
+      yield EntryType::class;
+  }
+}
